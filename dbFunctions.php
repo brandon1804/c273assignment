634b5c94173196ec2fee14b483562f87af16611f
brandon1804 <18044455@rp.edu.sg>
2021-01-12 14:41:06 +0800

    Made changes to dbFunctions.php

--- dbFunctions.php
+++ dbFunctions.php
@@ -1,38 +1,42 @@
 <?php
-/*
-Name: Brandon Koh Wei Sheng  Student ID: 18044455
 
-$HOST = 'localhost';
-$USERNAME = 'root';
-$PASSWORD = '';
-$DB = 'c273_assignment';
-$link = mysqli_connect($HOST,$USERNAME,$PASSWORD,$DB) or die(mysqli_connect_error());
-Name: Brandon Koh Wei Sheng  Student ID: 18044455
-*/
+if ($_SERVER['HTTP_HOST'] == "localhost"){
+$username = "root";
+$password = "";         // No password for localhost
+$db = "c273_assignment";
 
-
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+$host = "localhost";
+$link = mysqli_connect($host, $username, $password, $db) or
+        die(mysqli_connect_error());
 }
+else{
 
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+  $connectstr_dbhost = '';
+  $connectstr_dbname = '';
+  $connectstr_dbusername = '';
+  $connectstr_dbpassword = '';
+
+  foreach ($_SERVER as $key => $value) {
+  if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+  continue;
+  }
+
+  $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+  }
+
+  $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+  if (!$link) {
+  echo "Error: Unable to connect to MySQL." . PHP_EOL;
+  echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+  echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+  exit;
+  }
+ 
 }
 ?>
+
